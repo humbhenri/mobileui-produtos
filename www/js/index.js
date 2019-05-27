68f87406c424a1a064bb9b1f67dc7ad7db164b33
Humberto Pinheiro <humbhenri@gmail.com>
2019-05-27 14:28:22 -0300

    barcode scan

--- www/js/index.js
+++ www/js/index.js
@@ -3,12 +3,13 @@ var shortName = 'DBProdutos';
 var version = '1.0';
 var displayName = 'DBProdutos';
 var maxSize = 65535;
+var codbarras = '';
 
 var app = {
-    onDeviceReady: function () {
+    onDeviceReady: function() {
         app.startDB();
     },
-    startDB: function () {
+    startDB: function() {
         if (!window.openDatabase) {
             alert('Navegador não suporte WebSQL.');
             return;
@@ -16,18 +17,39 @@ var app = {
 
         db = openDatabase(shortName, version, displayName, maxSize);
 
-        db.transaction(function (tx) {
-            tx.executeSql('CREATE TABLE IF NOT EXISTS Produtos(Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Nome TEXT NOT NULL, Preco TEXT NOT NULL)');
+        db.transaction(function(tx) {
+            tx.executeSql(
+                'CREATE TABLE IF NOT EXISTS Produtos(Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Nome TEXT NOT NULL, Preco TEXT NOT NULL, Codbarras TEXT not null)'
+            );
             console.log('tabela criada');
-        })
+        });
     },
-    insere: function(){
-        db.transaction(function (tx) {
+    insere: function() {
+        db.transaction(function(tx) {
             var nome = document.getElementById('nome').value;
             var preco = document.getElementById('preco').value;
 
-            tx.executeSql('INSERT INTO Produtos(Nome,Preco) values("' + nome + '","' + preco + '")');
+            tx.executeSql(
+                `INSERT INTO Produtos(Nome,Preco, Codbarras) values('${nome}', '${preco}', ${codbarras})`
+            );
             console.log('inseriu');
+            codbarras = ''
+            document.getElementById('nome').value = '';
+            document.getElementById('preco').value = '';
         });
+    },
+    scan: function() {
+        console.log('scan');
+        cordova.plugins.barcodeScanner.scan(
+            function(result) {
+                if (result.text) {
+                    console.log(result.text);
+                    codbarras = result.text;
+                }
+            },
+            function(error) {
+                alert(error);
+            }
+        );
     }
-}
+};
